aeac67f4bc12cf0dd3f04adafdb9eccdcec08a06
Kovács Ferenc Richárd <kliberregobenedek@ktch.hu>
2024-05-15 09:41:19 +0200

    minden gomb megy hahhahaa

--- wpfMVVVM/ViewModel/MainViewModel.cs
+++ wpfMVVVM/ViewModel/MainViewModel.cs
@@ -1,31 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
+using System.Windows;
+using wpfMVVVM.Command;
 using wpfMVVVM.View;
 
 namespace wpfMVVVM.ViewModel
 {
-    public class MainViewModell
+    public class MainViewModell : INotifyPropertyChanged
     {
-		private object currentView;
+        private object currentView;
 
-		public object CurrentView
+        public object CurrentView
         {
-			get { return currentView; }
-			set { currentView = value; }
-		}
+            get { return currentView; }
+            set { currentView = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("CurrentView"));
+            }
+        }
 
-		private GreenView greenView;
-		private YellowView yellowView;
+        private GreenView greenView;
+        private YellowView yellowView;
+        private PurpleView purpleView;
 
-		public MainViewModell()
-		{
-			yellowView = new YellowView();
-			greenView = new GreenView();
-			CurrentView = greenView;
-		}
+        public event PropertyChangedEventHandler? PropertyChanged;
 
-	}
+        //protected void OnPropertyChanged()
+        //{
+        //    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("CurrentView"));
+        //}
+
+        public RelayCommand YellowViewCommand { get; }
+        public RelayCommand GreenViewCommand { get; }
+        public RelayCommand PurpleViewCommand { get; }
+        public RelayCommand KilepesEbbolACsodabol { get; }
+
+        public MainViewModell()
+        {
+            yellowView = new YellowView();
+            greenView = new GreenView();
+            purpleView = new PurpleView();
+            CurrentView = greenView;
+
+            YellowViewCommand = new RelayCommand(setYellow);
+            GreenViewCommand = new RelayCommand(x => CurrentView = greenView);
+            PurpleViewCommand = new RelayCommand(x => CurrentView = purpleView);
+            KilepesEbbolACsodabol = new RelayCommand(exitApp);
+
+        }
+
+        private void setYellow(object v)
+        {
+            CurrentView = yellowView;
+        }
+
+        private void exitApp(object v)
+        {
+            Application.Current.Shutdown();
+        }
+
+    }
 }
