af013cd9b8b47d91901bf2bdb5075cd445b0a1bb
Kovács Ferenc Richárd <kliberregobenedek@ktch.hu>
2024-05-15 08:59:40 +0200

    szinek foleg :flushed:

--- wpfMVVVM/MainWindow.xaml.cs
+++ wpfMVVVM/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using wpfMVVVM.ViewModel;
 
 namespace wpfMVVVM
 {
@@ -23,6 +24,7 @@ namespace wpfMVVVM
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = new MainViewModell();
         }
     }
 }
---
+++ wpfMVVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wpfMVVVM.View;
+
+namespace wpfMVVVM.ViewModel
+{
+    public class MainViewModell
+    {
+		private object currentView;
+
+		public object CurrentView
+        {
+			get { return currentView; }
+			set { currentView = value; }
+		}
+
+		private GreenView greenView;
+		private YellowView yellowView;
+
+		public MainViewModell()
+		{
+			yellowView = new YellowView();
+			greenView = new GreenView();
+			CurrentView = greenView;
+		}
+
+	}
+}
